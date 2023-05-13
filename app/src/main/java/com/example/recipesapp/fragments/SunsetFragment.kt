3aa39c35d28b49fdc576613b518eeeffdf0c940c
julia-bit <80833377+julia-bit@users.noreply.github.com>
2023-05-13 18:34:08 +0200

    add first cake animation

--- app/src/main/java/com/example/recipesapp/fragments/SunsetFragment.kt
+++ app/src/main/java/com/example/recipesapp/fragments/SunsetFragment.kt
@@ -2,45 +2,58 @@ package com.example.recipesapp.fragments
 
 import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateInterpolator
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.recipesapp.R
+import com.example.recipesapp.databinding.FragmentSunsetBinding
 
 class SunsetFragment : Fragment() {
 
-    private lateinit var mSceneView: View
-    private lateinit var mSunView: View
-    private lateinit var mSkyView: View
+    private lateinit var binding: FragmentSunsetBinding
 
-    companion object {
-        fun newInstance(): SunsetFragment {
-            return SunsetFragment()
-        }
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        binding = FragmentSunsetBinding.inflate(layoutInflater)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_sunset, container, false)
-        mSceneView = view
-        mSunView = view.findViewById(R.id.sun)
-        mSkyView = view.findViewById(R.id.sky)
 
-        mSceneView.setOnClickListener {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSunsetBinding.inflate(inflater, container, false)
+
+        binding.sun.setOnClickListener {
             startAnimation()
         }
 
-        return view
+        binding.sky.setOnClickListener {
+            findNavController().navigate(R.id.action_sunsetFragment_to_viewPagerFragment)
+
+        }
+
+        return binding.root
     }
 
     private fun startAnimation() {
-        val sunYStart = mSunView.top.toFloat()
-        val sunYEnd = mSkyView.height.toFloat()
-
-        val heightAnimator = ObjectAnimator.ofFloat(mSunView, "y", sunYStart, sunYEnd)
+        // Animate the sun
+        val sunYStart = binding.sun.top.toFloat()
+        val sunYEnd = 1150.toFloat()
+        val heightAnimator = ObjectAnimator.ofFloat(binding.sun, "y", sunYStart, sunYEnd)
             .setDuration(3000)
+        heightAnimator.interpolator = AccelerateInterpolator()
         heightAnimator.start()
+
 
     }
 
+
 }
+
